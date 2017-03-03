6a01c4c9e69ce169fa7513c85092997d7a835303
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-03 14:18:15 -0800

    Add About page to Welcome controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
+
+  get 'welcome/about'
+
 end
+
+
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
