daa2f5ef90922669c797c5b0fc0169870f2c221a
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-05 13:42:35 -0800

    Create Posts controller; refactor routes & application template, create index action tests and controller method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :posts  # This succinct syntax replaces the four lines below and creates post CRUD routes for creating, updating, viewing, and deleting instances of Post.
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
+
+  get 'about' => 'welcome#about'  # Replaces "get 'welcome/about' to shorter URI of '/about' while maintaining the controller#action relationship"
+  get 'faq' => 'welcome#faq'
+  get 'why' => 'welcome#why'
+
   root to: "welcome#index"
-
-  get 'welcome/about'
-
-  get 'welcome/faq'
-
-  get 'welcome/why'
 
 end
 
