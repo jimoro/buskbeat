c5b3951628aface9e0dda50ac771170f0229fd95
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-05 00:03:20 -0800

    Create methods for seeds.rb, create random_data.rb module, update application.rb to add 'lib' to autoload_paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
+require 'random_data'
+
+# Create posts
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+# Create comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
