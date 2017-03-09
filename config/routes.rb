4b6440d72555aa6a2e6ab5b9e4ee0c9624217179
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-09 11:19:25 -0800

    Add Topics model, controller, and views; refactor routes and Posts model, controller, and views to nest under Topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :posts  # This succinct syntax replaces the four lines below and creates post CRUD routes for creating, updating, viewing, and deleting instances of Post.
+  resources :topics do
+    resources :posts, except: [:index]  # This succinct syntax (prior to refactoring to nest under 'topics' and exclude 'index') replaces the four lines below and creates post CRUD routes for creating, updating, viewing, and deleting instances of Post.
+  end
   # get 'posts/index'
   # get 'posts/show'
   # get 'posts/new'
-  # get 'posts/edit'
+  # get 'posts/edit'  
 
   get 'about' => 'welcome#about'  # Replaces "get 'welcome/about' to shorter URI of '/about' while maintaining the controller#action relationship"
   get 'faq' => 'welcome#faq'
