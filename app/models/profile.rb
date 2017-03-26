63516ea6a154d15bcb184d8af9b3baa5ef16d739
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-25 18:52:02 -0700

    Add profile image icon to signed in nav bar, add conditional logic to attempt to display avatar image only if file exists on both profile show view and on global nav

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,8 +2,10 @@ class Profile < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
   # Paperclip avatar
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", icon: "32x32>" }, default_url: "/images/:style/missing"
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_with AttachmentSizeValidator, attributes: :avatar, less_than: 1.megabytes
 
   validates :act, length: { minimum: 2 }, presence: true
   validates :bio, length: { minimum: 20 }, presence: true
